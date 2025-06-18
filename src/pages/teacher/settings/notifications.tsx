6b0ba1ff28fcae3c303124c7003d98a396674d21
javier <nhat1234559@gmail.com>
2025-06-18 22:35:11 +0700

    update payment history for teacher

--- src/pages/teacher/settings/notifications.tsx
+++ src/pages/teacher/settings/notifications.tsx
@@ -5,7 +5,7 @@ export default function NotificationsPage() {
   return (
     <div className="space-y-6 w-full">
       <div>
-        <h3 className="text-lg font-medium">Notifications</h3>
+        <h3 className="text-lg font-medium">Thông báo</h3>
         <p className="text-sm text-muted-foreground">Thông báo của bạn</p>
       </div>
       <Separator />
--- src/pages/teacher/settings/payment.tsx
+++ src/pages/teacher/settings/payment.tsx
@@ -1,5 +1,19 @@
 import TeacherBalanceView from "@/feature/token/views/TeacherBalanceView";
+import HistoryTableBalanceView from "@/feature/token/views/HistoryTableBalanceView";
 
 export default function Page() {
-  return <TeacherBalanceView />;
+  return (
+    <div className="p-4 mx-auto">
+      <div>
+        <h3 className="text-lg font-medium">Thanh toán</h3>
+        <p className="text-sm text-muted-foreground">Thanh toán của bạn</p>
+      </div>
+      <div className="flex flex-col my-6 gap-4">
+        <TeacherBalanceView />
+      </div>
+      <div className="flex flex-col my-6 gap-4">
+        <HistoryTableBalanceView />
+      </div>
+    </div>
+  );
 }
